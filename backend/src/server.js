313a9522fb27cfbda8e984d8b44b80d496520d02
Yohannes Alemayehu <maamiyee1210@gmail.com>
2026-07-01 02:40:31 +0300

    cron.js added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,13 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import job from './config/cron.js'
  
 const app = express();
+const PORT = process.env.PORT ||5001;
+if (ENV.NODE_ENV === "production") job.start();
 
-const PORT = process.env.PORT ||8001;
+
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ success: true });
